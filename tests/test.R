d37771c946d228d7d3696d57718d87028e059b35
Rich FitzJohn <rich.fitzjohn@gmail.com>
2016-04-01 09:48:21 -0700

    Bugfix in the schema, beginnings of (non-automatic) tests

---
+++ tests/test.R
@@ -0,0 +1,25 @@
+## These are tests of the schema.  We'll use the jsonvalidate package
+## to run this, but it could be run happily from node if I spoke js :)
+library(testthat)
+v <- jsonvalidate::json_validator("../schema.json")
+
+read_lines <- function(x) paste(readLines(x), collapse="\n")
+to_json <- function(x, exclude=NULL, include=NULL) {
+  unbox <- c(setdiff(c("name", "github_user", "description", "date", "type"),
+                     exclude), include)
+  i <- names(x) %in% unbox
+  x[i] <- lapply(x[i], jsonlite::unbox)
+  jsonlite::toJSON(x)
+}
+
+expect_true(v("minimal.json"))
+
+## Let's start messing with this and check that things do break.  But
+## first, generate valid json:
+d <- jsonlite::fromJSON(read_lines("minimal.json"))
+expect_true(v(to_json(d)))
+
+## authors must be a vector
+expect_false(v(to_json(d, NULL, "authors")))
+## authors must be at least one item
+expect_false(v(to_json(modifyList(d, list(authors=character(0))))))
