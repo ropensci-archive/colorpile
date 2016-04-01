3bbe87d3db4d4baee1cf5dcfed9a8154fe6dd732
Mika Braginsky <mikabr@stanford.edu>
2016-04-01 12:06:40 -0700

    colour brewer palettes

--- tests/test-schema.R
+++ tests/test-schema.R
@@ -1,7 +1,7 @@
 library(testthat)
 v <- jsonvalidate::json_validator("../schema.json")
 
-read_lines <- function(x) paste(readLines(x), collapse="\n")
+read_lines <- function(x) paste(readLines(x), collapse = "\n")
 to_json <- function(x, exclude=NULL, include=NULL) {
   unbox <- c(setdiff(c("name", "github_user", "description", "date", "type"),
                      exclude), include)
@@ -9,6 +9,13 @@ to_json <- function(x, exclude=NULL, include=NULL) {
   x[i] <- lapply(x[i], jsonlite::unbox)
   jsonlite::toJSON(x)
 }
+
+expect_silent(v("minimal.json"))
+palette_files <- list.files("../palettes")
+purrr::walk(palette_files, function(palette_file) {
+  cat(palette_file, "\n")
+  expect_silent(v(file.path("../palettes", palette_file), verbose = TRUE, error = TRUE))
+})
 
 ## Let's start messing with this and check that things do break.  But
 ## first, generate valid json:
@@ -18,4 +25,4 @@ expect_true(v(to_json(d)))
 ## authors must be a vector
 expect_false(v(to_json(d, NULL, "authors")))
 ## authors must be at least one item
-expect_false(v(to_json(modifyList(d, list(authors=character(0))))))
+expect_false(v(to_json(modifyList(d, list(authors = character(0))))))
